7ddc08fd0d87118578ba530a8c2769d6dc4caa09
Lívia M. Xavier <livmedeiros03@hotmail.com>
2024-03-06 18:20:25 -0300

    Little improvement in board printing

--- src/boardGame/BoardException.java
+++ src/boardGame/BoardException.java
@@ -1,10 +1,7 @@
 package boardGame;
 
-import java.io.Serial;
-
 public class BoardException extends RuntimeException {
 
-    @Serial
     private static final long serialVersionUID = 1L;
 
     public BoardException(String msg){
